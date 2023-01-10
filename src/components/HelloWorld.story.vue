14f79deec2651f9c008423ea3de7069804c1a66c
kbtaniguchi <k_taniguchi@actier.co.jp>
2023-01-10 17:57:52 +0900

    histoireを導入

---
+++ src/components/HelloWorld.story.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue"
+</script>
+
+<template>
+  <Story>
+    <Variant title="朝の挨拶">
+      <HelloWorld msg="おはよう！"/>
+    </Variant>
+    <Variant title="お昼の挨拶">
+      <HelloWorld msg="こんにちは！"/>
+    </Variant>
+    <Variant title="夜の挨拶">
+      <HelloWorld msg="こんにちは！"/>
+    </Variant>
+  </Story>
+</template>
