c340a0a2e5ff66d8928006706044e72c73d0d735
kbtaniguchi <k_taniguchi@actier.co.jp>
2023-01-11 17:41:32 +0900

    viteのライブラリモードの設定を入れる
    refs. https://ja.vitejs.dev/guide/build.html#%E3%83%A9%E3%82%A4%E3%83%96%E3%83%A9%E3%83%AA%E3%83%A2%E3%83%BC%E3%83%89

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import {resolve} from 'path'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/main.ts'),
+            name: 'jupiter',
+            fileName: 'jupiter'
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+    },
 })
