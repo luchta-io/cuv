934bda6fe0fac5aa459fa587c36b15ca463ba91b
kbtaniguchi <k_taniguchi@actier.co.jp>
2023-01-18 13:35:05 +0900

    Revert "viteのライブラリモードの設定を入れる"
    This reverts commit c340a0a2e5ff66d8928006706044e72c73d0d735.

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,14 @@
-import {resolve} from 'path'
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/main.ts'),
-            name: 'jupiter',
-            fileName: 'jupiter'
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue',
-                },
-            },
-        },
-    },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
