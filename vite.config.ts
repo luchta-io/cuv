a73a0fa30ecdb9e562c27245833524e23b182513
kbtaniguchi <k_taniguchi@actier.co.jp>
2023-01-26 15:16:02 +0900

    vite.config.tsのassetFileNamesの設定のせいでhistoireのビルドがコケるようになっていたので修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
-import { resolve } from 'path'
+import {fileURLToPath, URL} from 'node:url'
+import {resolve} from 'path'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'Jupiter',
-      fileName: 'jupiter'
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'Jupiter',
+            fileName: 'jupiter'
         },
-        assetFileNames: chunkInfo => {
-          // refs. https://github.com/vitejs/vite/issues/4863
-          if (chunkInfo.name === 'style.css') return 'jupiter.css';
-          return ""
-        }
-      },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                }
+            },
+        },
     },
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
