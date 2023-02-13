aee4d02fb95f6e151ff54b063139a68f071d7b80
riyo kondo <r_kondo@actier.co.jp>
2023-02-13 17:17:20 +0900

    TextInput部品のファイルを作成

---
+++ src/components/form/CTextInput.story.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+import CTextInput from "@/components/form/CTextInput.vue";
+
+const data: {
+    入力値: string
+} = reactive({
+    入力値: '',
+})
+</script>
+
+<template>
+<Story
+    title="Form / Input"
+    :layout="{ type: 'grid', width: 200 }"
+>
+    <Variant title="基本" auto-props-disabled>
+        <c-text-input 
+            v-model="data.入力値"
+        />
+        <template #controls>
+        </template>
+    </Variant>
+
+    <Variant title="非活性">
+    </Variant>
+
+    <Variant title="長いコンテンツ">
+    </Variant>
+
+</Story>
+</template>
+
+<docs lang="md">
+# TextInput
+
+
+## Props
+
+| Name | Type | Default | Description |
+| --- | --- | --- | --- |
+
+## Slots
+
+| Name | Props (if scoped) | Description |
+| --- | --- | --- |
+
+## Events
+
+| Name | Parameters | Description |
+| --- | --- | --- |
+</docs>
---
+++ src/components/form/CTextInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {computed} from 'vue';
+
+const props = withDefaults(defineProps<{
+    modelValue: string
+}>(), {
+})
+
+const emits = defineEmits<{
+    (e: 'update:modelValue', value: string): void
+}>()
+
+const 入力値 = computed({
+    get: () => props.modelValue,
+    set: value => {
+        emits('update:modelValue', value)
+    }
+})
+</script>
+
+<template>
+<input 
+v-model="入力値"
+type="text"
+class=""/>
+</template>
