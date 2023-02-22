b8e49fe5ce78701cd066c3876618b070822e8989
riyo kondo <r_kondo@actier.co.jp>
2023-02-22 14:09:44 +0900

    switcherのhistoireを変更

--- src/components/layout/CSwitcher.story.vue
+++ src/components/layout/CSwitcher.story.vue
@@ -21,11 +21,11 @@ const data: {
     <Story title="Layout / CSwitcher" :layout="{ type: 'grid', width: '100%' }">
         <Variant title="threshold" auto-props-disabled>
             <c-switcher
-                :threshold="data.threshold"
-                class="with-outline"
+            :threshold="data.threshold"
+            :limit="6"
+            class="with-outline"
             >
-                <c-box v-for="n in 4" :key="n" bordered>
-                    Box{{ n }}
+                <c-box v-for="n in 6" :key="n" bordered padding="none" class="w-full max-w-lg h-10 bg-yellow-200">
                 </c-box>
             </c-switcher>
             <template #controls>
@@ -37,7 +37,7 @@ const data: {
                 :space="data.space"
                 class="with-outline bg-yellow-200"
             >
-                <c-box v-for="n in 4" :key="n" bordered class="bg-white">
+                <c-box v-for="n in 3" :key="n" bordered class="bg-white">
                     Box{{ n }}
                 </c-box>
             </c-switcher>
@@ -69,6 +69,8 @@ const data: {
 <docs lang="md">
 # CSwitcher
 水平方向のレイアウトと垂直方向のレイアウトを切り替えたい時に有用なレイアウトコンポーネントです。
+水平方向のレイアウトの場合、入れ子になる各要素は、水平方向のスペースを同じ比率で占めます。
+※コンテナのブレイクポイント(threshold)よりも、入れ子の各要素の最小幅の合計が大きくならないように気を付けてください。
 
 ## Props
 
