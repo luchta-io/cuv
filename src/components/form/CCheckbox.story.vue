fb3c0e4dae94974bac04341b34ade0a05daa3910
riyo kondo <r_kondo@actier.co.jp>
2023-02-27 14:04:30 +0900

    Checkboxのファイルを作成

---
+++ src/components/form/CCheckbox.story.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+import CCheckbox from "@/components/form/CCheckbox.vue";
+
+const data: {
+} = reactive({
+})
+</script>
+
+<template>
+<Story
+    title="Form / CButton"
+    :layout="{ type: 'grid', width: 200 }"
+>
+
+    <Variant title="基本" auto-props-disabled>
+        <template #controls>
+        </template>
+    </Variant>
+
+</Story>
+</template>
+
+<docs lang="md">
+# CCheckbox
+
+## Props
+
+| Name | Type | Default | Description |
+| --- | --- | --- | --- |
+
+## Slots
+
+| Name | Props (if scoped) | Description |
+| --- | --- | --- |
+
+## Events
+
+| Name | Parameters | Description |
+| --- | --- | --- |
+| - | - | このコンポーネント独自のイベントはありません |
+</docs>
