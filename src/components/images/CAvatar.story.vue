d34eeee5ad8e3123287af547c815e66b5a888bfc
riyo kondo <r_kondo@actier.co.jp>
2023-05-31 16:09:55 +0900

    コンポーネントを作成

---
+++ src/components/images/CAvatar.story.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const data: {
+} = reactive({
+})
+</script>
+
+<template>
+<Story
+    title="Images & Icons / CAvatar"
+    :layout="{ type: 'grid', width: '100%' }"
+>
+    <Variant title="基本" auto-props-disabled>
+        <template #controls>
+        </template>
+    </Variant>
+</Story>
+</template>
+
+<docs lang="md">
+# CAvatar
+プロフィール写真を表示する際など、コンテナの中にコンテンツを表示するためのコンポーネントです。
+
+## Props
+
+| Name | Type | Default | Description |
+| --- | --- | --- | --- |
+| icon | string | '' | iconのpathを指定します |
+| size | "small"/"medium"/"large" | "medium" | アイコンのサイズを指定します |
+
+
+## Slots
+
+| Name | Props (if scoped) | Description |
+| --- | --- | --- |
+| - | - | このコンポーネント独自のSlotはありません |
+
+## Events
+
+| Name | Parameters | Description |
+| --- | --- | --- |
+| - | - | このコンポーネント独自のイベントはありません |
+</docs>
---
+++ src/components/images/CAvatar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+type ColorType =
+    'white' | 'black' | 'light' | 'dark' | 
+    'primary' | 'link' |
+    'success' | 'danger' | 'warning' | 'info'
+
+const props = withDefaults(defineProps<{
+    color?: ColorType
+    size?: "small" | "medium" | "large"
+}>(), {
+    color: 'light',
+    size: 'medium',
+})
+
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
