38f1f751d25c5a9ecde1ddd3a42cd98c54fa9785
riyo kondo <r_kondo@actier.co.jp>
2023-06-02 11:04:44 +0900

    Avatarコンポーネントを作る

--- src/components/images/CAvatar.story.vue
+++ src/components/images/CAvatar.story.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import {reactive} from "vue";
+import { computed, reactive} from "vue";
+import { mdiAccount, mdiWifi, mdiAirplane } from '@mdi/js';
+import CAvatar from "@/components/images/CAvatar.vue";
+import CCluster from "@/components/layout/CCluster.vue";
+import CStack from "@/components/layout/CStack.vue";
+import CSvgIcon from "@/components/images/CSvgIcon.vue";
+import CBox from "@/components/layout/CBox.vue";
 
 const data: {
+    isIcon: boolean
+    size: string
+    color: 'white' | 'black' | 'light' | 'dark' | 'primary' | 'link' | 'success' | 'danger' | 'warning' | 'info' | undefined
+    isImage: boolean
+    variant: 'text' | 'flat' | 'elevated' | 'outlined' | 'plain'
 } = reactive({
+    isIcon: false,
+    size: 'medium',
+    color: 'light',
+    isImage: false,
+    variant: 'flat',
 })
+
+const image = computed(() => {
+    return data.isImage ? '/cuv/src/assets/sample_Parakeet.png' : undefined
+})
+
+const icon = computed(() => {
+    return data.isIcon ? mdiAccount : undefined
+})
+
 </script>
 
 <template>
@@ -12,8 +37,94 @@ const data: {
     :layout="{ type: 'grid', width: '100%' }"
 >
     <Variant title="基本" auto-props-disabled>
+        <CBox>
+            <CCluster justify="center">
+                <CAvatar
+                    :color="data.color"
+                    :icon="icon"
+                    :size="data.size"
+                    :image="image"
+                    :variant="data.variant"
+                />
+            </CCluster>
+        </CBox>
         <template #controls>
+            <HstSelect
+                v-model="data.color"
+                title="color"
+                :options="[
+                            {value: undefined, label: 'なし(undefined)'},
+                            {value: 'white', label: 'white'},
+                            {value: 'black', label: 'black'},
+                            {value: 'light', label: 'light'},
+                            {value: 'dark', label: 'dark'},
+                            {value: 'primary', label: 'primary'},
+                            {value: 'link', label: 'link'},
+                            {value: 'success', label: 'success'},
+                            {value: 'danger', label: 'danger'},
+                            {value: 'warning', label: 'warning'},
+                            {value: 'info', label: 'info'},
+                        ]"
+            />
+            <HstCheckbox v-model="data.isIcon" title="icon"/>
+            <HstCheckbox v-model="data.isImage" title="image"/>
+            <HstText v-model="data.size" title="size"/>
+            <HstSelect
+                v-model="data.variant"
+                title="variant"
+                :options="[
+                            {value: 'text', label: 'text'},
+                            {value: 'flat', label: 'flat'},
+                            {value: 'elevated', label: 'elevated'},
+                            {value: 'outlined', label: 'outlined'},
+                            {value: 'plain', label: 'plain'},
+                        ]"
+            />
         </template>
+    </Variant>
+    <Variant title="サイズ" auto-props-disabled>
+        <CCluster justify="space-between" align="center">
+            <CStack class="flex flex-col items-center">
+                <CAvatar size="x-small" color="dark"/>
+                x-small
+            </CStack>
+            <CStack class="flex flex-col items-center">
+                <CAvatar size="small" color="dark"/>
+                small
+            </CStack>
+            <CStack class="flex flex-col items-center">
+                <CAvatar color="dark"/>
+                medium
+            </CStack>
+            <CStack class="flex flex-col items-center">
+                <CAvatar size="large" color="dark"/>
+                large
+            </CStack>
+            <CStack class="flex flex-col items-center">
+                <CAvatar size="x-large" color="dark"/>
+                x-large
+            </CStack>
+        </CCluster>
+    </Variant>
+    <Variant title="タイル" auto-props-disabled>
+        <CCluster justify="center">
+            <CAvatar color="link" rounded="0">
+                <CSvgIcon :icon="mdiWifi" class="text-white"/>
+            </CAvatar>
+        </CCluster>
+    </Variant>
+    <Variant title="Slot" auto-props-disabled>
+        <CCluster justify="space-between">
+            <CAvatar color="primary">
+                <CSvgIcon :icon="mdiAirplane"/>
+            </CAvatar>
+            <CAvatar color="light">
+                <img src="/cuv/src/assets/sample_cat.png"/>
+            </CAvatar>
+            <CAvatar color="warning">
+                CUV
+            </CAvatar>
+        </CCluster>
     </Variant>
 </Story>
 </template>
@@ -26,15 +137,18 @@ const data: {
 
 | Name | Type | Default | Description |
 | --- | --- | --- | --- |
-| icon | string | '' | iconのpathを指定します |
-| size | "small"/"medium"/"large" | "medium" | アイコンのサイズを指定します |
-
+| color | 'white' / 'black' / 'light' / 'dark' / 'primary' / 'link' / 'success' / 'danger' / 'warning' / 'info' | undefined | 色を指定します |
+| icon | string | undefined | iconのpathを指定します |
+| image | string | undefined | 画像のsrcに渡すpathを指定します |
+| rounded | string / number / boolean | false | コンポーネントの`border-radius`を指定します |
+| size | string/number | "medium" | サイズを指定します。`x-small`, `small`, `medium`, `large`, `x-large`で指定もできます。 |
+| variant | 'text' / 'flat' / 'elevated' / 'outlined' / 'plain' | 'flat' | コンポーネントに個別のスタイルを適用します |
 
 ## Slots
 
 | Name | Props (if scoped) | Description |
 | --- | --- | --- |
-| - | - | このコンポーネント独自のSlotはありません |
+| default| - | このコンポーネント内に表示するコンテンツを指定します |
 
 ## Events
 
--- src/components/images/CAvatar.vue
+++ src/components/images/CAvatar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import CSvgIcon from '@/components/images/CSvgIcon.vue'
 
 type ColorType =
     'white' | 'black' | 'light' | 'dark' | 
@@ -7,17 +9,101 @@ type ColorType =
 
 const props = withDefaults(defineProps<{
     color?: ColorType
-    size?: "small" | "medium" | "large"
+    icon?: string
+    image?: string
+    rounded?: string | number | boolean
+    size?: string|number
+    variant?: 'text' | 'flat' | 'elevated' | 'outlined' | 'plain'
 }>(), {
-    color: 'light',
+    rounded: false,
     size: 'medium',
+    variant: 'flat',
 })
 
+const backgroundColor = computed(() => {
+    if ( props.color === 'white' ) return 'bg-[var(--cuv-white)]'
+    if ( props.color === 'black' ) return 'bg-[var(--cuv-black)]'
+    if ( props.color === 'light' ) return 'bg-[var(--cuv-light-background)] hover:bg-[var(--cuv-light-background-hover)]'
+    if ( props.color === 'dark' )  return 'bg-[var(--cuv-dark-background)] hover:bg-[var(--cuv-dark-background-hover)] text-white'
+    if ( props.color === 'primary' ) return 'bg-[var(--cuv-primary-background)] hover:bg-[var(--cuv-primary-background-hover)] text-white'
+    if ( props.color === 'link' ) return 'bg-[var(--cuv-link-background)] hover:bg-[var(--cuv-link-background-hover)] text-white'
+    if ( props.color === 'success' ) return 'bg-[var(--cuv-success-background)] hover:bg-[var(--cuv-success-background-hover)] text-white'
+    if ( props.color === 'danger' ) return 'bg-[var(--cuv-danger-background)] hover:bg-[var(--cuv-danger-background-hover)] text-white'
+    if ( props.color === 'warning' ) return 'bg-[var(--cuv-warning-background)] hover:bg-[var(--cuv-warning-background-hover)]'
+    if ( props.color === 'info' ) return 'bg-[var(--cuv-info-background)] hover:bg-[var(--cuv-info-background-hover)] text-white'
+    return 'bg-transparent'
+})
+
+const outlineColor = computed(() => {
+    if ( props.color === 'white' ) return 'outline outline-[var(--cuv-white)]'
+    if ( props.color === 'black' ) return 'outline outline-[var(--cuv-black)]'
+    if ( props.color === 'light' ) return 'outline outline-[var(--cuv-light-outline)] hover:outline-[var(--cuv-light-outline-hover)]'
+    if ( props.color === 'dark' )  return 'outline outline-[var(--cuv-dark-outline)] hover:outline-[var(--cuv-dark-outline-hover)]'
+    if ( props.color === 'primary' ) return 'outline outline-[var(--cuv-primary-outline)] hover:outline-[var(--cuv-primary-outline-hover)]'
+    if ( props.color === 'link' ) return 'outline outline-[var(--cuv-link-outline)] hover:outline-[var(--cuv-link-outline-hover)]'
+    if ( props.color === 'success' ) return 'outline outline-[var(--cuv-success-outline)] hover:outline-[var(--cuv-success-outline-hover)]'
+    if ( props.color === 'danger' ) return 'outline outline-[var(--cuv-danger-outline)] hover:outline-[var(--cuv-danger-outline-hover)]'
+    if ( props.color === 'warning' ) return 'outline outline-[var(--cuv-warning-outline)] hover:outline-[var(--cuv-warning-outline-hover)]'
+    if ( props.color === 'info' ) return 'outline outline-[var(--cuv-info-outline)] hover:outline-[var(--cuv-info-outline-hover)]'
+    return 'outline-transparent'
+})
+
+const textColor = computed(() => {
+    if ( props.color === 'white' ) return 'text-[var(--cuv-white)]'
+    if ( props.color === 'black' ) return 'text-[var(--cuv-black)]'
+    if ( props.color === 'light' ) return 'text-[var(--cuv-light-text)]'
+    if ( props.color === 'dark' )  return 'text-[var(--cuv-dark-text)]'
+    if ( props.color === 'primary' ) return 'text-[var(--cuv-primary-text)]'
+    if ( props.color === 'link' ) return 'text-[var(--cuv-link-text)]'
+    if ( props.color === 'success' ) return 'text-[var(--cuv-success-text)]'
+    if ( props.color === 'danger' ) return 'text-[var(--cuv-danger-text)]'
+    if ( props.color === 'warning' ) return 'text-[var(--cuv-warning-text)]'
+    if ( props.color === 'info' ) return 'text-[var(--cuv-info-text)]'
+    return 'text-black'
+})
+
+const avatarClass = computed(() => {
+    const base = [
+        'rounded-full flex inline-flex items-center justify-center leading-normal overflow-hidden relative text-center align-middle',
+        `transition-[${fixedSize.value}]`,
+    ]
+
+    if ( props.variant === 'text' ) base.push('bg-transparent', textColor.value)
+    if ( props.variant === 'flat' ) base.push(backgroundColor.value)
+    if ( props.variant === 'elevated' ) base.push('shadow-md',backgroundColor.value)
+    if ( props.variant === 'outlined' ) base.push(outlineColor.value, textColor.value)
+    if ( props.variant === 'plain' ) base.push('opacity-60', textColor.value)
+
+    return base
+})
+
+const fixedSize = computed(() => {
+    if ( props.size === 'x-small' ) return '1.5rem'
+    if ( props.size === 'small' ) return '2rem'
+    if ( props.size === 'medium' ) return '2.5rem'
+    if ( props.size === 'large' ) return '3.5rem'
+    if ( props.size === 'x-large' ) return '4.5rem'
+    if ( !Number.isNaN(props.size) ) return props.size+'px'
+
+    return props.size
+})
+
+const fixedRounded = computed(() => {
+    if ( !props.rounded ) return '50%'
+    if ( !Number.isNaN(props.rounded) ) return props.rounded+'px'
+    if ( typeof props.rounded === 'string' ) return props.rounded
+    return '50%'
+})
 
 </script>
 
 <template>
-    <div>
-        
+    <div :class="avatarClass" :style="{width: fixedSize, height: fixedSize, 'border-radius': fixedRounded}">
+        <CSvgIcon v-if="icon && !image" :icon="icon" size="large"/>
+        <div v-if="image" class="object-cover w-full h-full">
+            <img :src="image"/>
+        </div>
+        <slot />
     </div>
 </template>
+
