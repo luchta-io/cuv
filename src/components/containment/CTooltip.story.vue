4cc9e0aee58763d8f767aa60bb49f623fc45ddff
riyo kondo <r_kondo@actier.co.jp>
2023-06-07 10:25:55 +0900

    Tooltipコンポーネントを作る

--- src/components/containment/CTooltip.story.vue
+++ src/components/containment/CTooltip.story.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import {reactive} from "vue";
+import { mdiCart } from '@mdi/js';
 import CButton from "@/components/containment/CButton.vue";
 import CTooltip from "@/components/containment/CTooltip.vue";
+import CBox from "@/components/layout/CBox.vue";
+import CCluster from "@/components/layout/CCluster.vue";
+import CStack from "@/components/layout/CStack.vue";
+import CSvgIcon from "@/components/images/CSvgIcon.vue";
+import CAvatar from "@/components/images/CAvatar.vue";
 
 const data: {
-    color: 'white' | 'black' | 'light' | 'dark' | 'primary' | 'link' | 'success' | 'danger' | 'warning' | 'info'
-    outlined: boolean
+    text: string
+    modelValue: boolean
+    disabled: boolean
 } = reactive({
-    color: 'light',
-    outlined: false,
+    text: 'Tooltip',
+    modelValue: false,
+    disabled: false,
+})
+
+const custom: {
+    show: boolean
+} = reactive({
+    show: false,
 })
 </script>
 
@@ -18,13 +32,66 @@ const data: {
     :layout="{ type: 'grid', width: '100%' }"
 >
     <Variant title="基本" auto-props-disabled>
-        <CTooltip msg="Tooltip">
-            <CButton>Tooltipを表示</CButton>
-        </CTooltip>
+        <CBox padding="large">
+            <CCluster justify="center">
+                <CTooltip :text="data.text" v-model="data.modelValue" :disabled="data.disabled">
+                    <template #activator>
+                        <CButton>Tooltipを表示</CButton>
+                    </template>
+                </CTooltip>
+            </CCluster>
+        </CBox>
         <template #controls>
+            <HstText v-model="data.text" title="text"/>
+            <HstCheckbox v-model="data.modelValue" title="modelValue"/>
+            <HstCheckbox v-model="data.disabled" title="disabled"/>
         </template>
     </Variant>
-
+    <Variant title="位置" auto-props-disabled>
+        <CBox padding="large">
+            <CCluster justify="space-around">
+                <CTooltip text="Tooltip" location="start">
+                    <template #activator>
+                        <CButton>START</CButton>
+                    </template>
+                </CTooltip>
+                <CTooltip text="Tooltip" location="end">
+                    <template #activator>
+                        <CButton>END</CButton>
+                    </template>
+                </CTooltip>
+                <CTooltip text="Tooltip" location="top">
+                    <template #activator>
+                        <CButton>TOP</CButton>
+                    </template>
+                </CTooltip>
+                <CTooltip text="Tooltip" location="bottom">
+                    <template #activator>
+                        <CButton>BOTTOM</CButton>
+                    </template>
+                </CTooltip>
+            </CCluster>
+        </CBox>
+    </Variant>
+    <Variant title="カスタム" auto-props-disabled>
+        <CBox padding="large">
+            <CStack>
+                <CCluster justify="center">
+                    <CButton @click="custom.show = !custom.show">TOGGLE</CButton>
+                </CCluster>
+                <CCluster justify="center" class="pt-10">
+                    <CTooltip v-model="custom.show" location="top">
+                        <template #activator>
+                            <CAvatar color="light" variant="elevated">
+                                <CSvgIcon :icon="mdiCart"/>
+                            </CAvatar>
+                        </template>
+                        <span>Tooltip</span>
+                    </CTooltip>
+                </CCluster>
+            </CStack>    
+        </CBox>
+    </Variant>
 </Story>
 </template>
 
@@ -36,20 +103,22 @@ const data: {
 
 | Name | Type | Default | Description |
 | --- | --- | --- | --- |
-| color | 'white' \| 'black' \| 'light' \| 'dark' \| 'primary' \| 'link' \| 'success' \| 'danger' \| 'warning' \| 'info' | 'light' | ボタンの色を指定します |
-| outlined | boolean | false | ボタンの見た目をアウトラインボタンにする場合は指定します |
+| disabled | boolean | false | コンポーネントをクリックまたはターゲットにする機能を削除する場合は指定します |
 | id | string | undefined | idを指定します |
-| name | string | undefined | nameを指定します |
+| location | 'start' \| 'end' \| 'top' \| 'bottom' | 'end' | コンテンツに対してtooltipを表示する位置を指定します |
+| modelValue | boolean | false | コンポーネントのv-model値。tooltipの表示非表示を手動で切り替えることができます |
+| text | string | undefined | コンポーネントのコンテンツテキストを指定します |
 
 ## Slots
 
 | Name | Props (if scoped) | Description |
 | --- | --- | --- |
-| default | - | ボタンに表示するコンテンツを指定します |
+| default | - | tooltip内に表示するコンテンツを指定します |
+| activator | - | tooltipをアウティブにするためのコンテンツを指定します |
 
 ## Events
 
 | Name | Parameters | Description |
 | --- | --- | --- |
-| - | - | このコンポーネント独自のイベントはありません |
+| update:modelValue | - | コンポーネントのv-modelが変更されたときに発行されるイベントです |
 </docs>
--- src/components/containment/CTooltip.vue
+++ src/components/containment/CTooltip.vue
@@ -1,73 +1,134 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
 
 const props = withDefaults(defineProps<{
-    msg: string
-    position?: 'top'|'bottom'|'right'|'left'
+    disabled?: boolean
+    id?: string
+    location?: 'start' | 'end' | 'top' | 'bottom'
+    modelValue?: boolean
+    text?: string
 }>(), {
-    position: 'top',
+    disabled: false,
+    location: 'end',
+    modelValue: false
 })
+
+const emits = defineEmits<{
+    (e: 'update:modelValue', value: any): void
+}>()
 
 const data: {
     isActive: boolean
 } = reactive({
-    isActive: false
+    isActive: false,
 })
 
+const isActive = computed({
+    get: () => props.modelValue || data.isActive,
+    set: value => {
+        if ( props.disabled ) return false
+        emits('update:modelValue', value)
+        console.log(value)
+        data.isActive = value
+    }
+})
+
+const computedClass = computed(()=> {
+    const base = [
+        "w-auto",
+        "max-w-[10rem]",
+        "rounded",
+        "bg-black",
+        "px-2",
+        "py-1",
+        "text-white",
+        "text-xs",
+        "inline-block",
+        "absolute",
+        "break-all",
+        isActive.value ? 'block' : 'invisible',
+    ]
+    return base
+})
+
+const uid = Math.random().toString(32).substring(2)
+
+const id = computed(() => 'c-tooltip--' + uid)
+
+const tooltipId = computed(() => 'c-tooltip-content--' + uid)
+
 const open = () => {
-    data.isActive = true
-    console.log('open', data.isActive)
+    if ( props.disabled ) return 
+    emits('update:modelValue', true)
+    isActive.value = true
+    console.log('open', isActive.value)
 }
 
 const close = () => {
-    data.isActive = false
+    if ( props.disabled ) return 
+    emits('update:modelValue', false)
+    isActive.value = false
 }
 
-const styles = () => {
-    if(props.position==='top') return {inset:'auto auto calc(100% + 7px) 50%',transform:'translateX(-50%)'}
-    if(props.position==='bottom') return {inset:'calc(100% + 7px) auto auto 50%',transform:'translateX(-50%)'}
-    if(props.position==='right') return {inset:'50% auto auto calc(100% + 7px)',transform:'translateY(-50%)'}
-    if(props.position==='left') return {inset:'50% calc(100% + 7px) auto auto',transform:'translateY(-50%)'}
+const teleportTarget = computed(() => {        
+    const targetElement = document.body
+    const container = document.createElement('div')
+    container.className = 'c-overlay-container'
+    targetElement.appendChild(container)
+    return container
+})
+
+const tooltipHeight = computed(() => {
+    const tooltipElement = document.getElementById(tooltipId.value)
+    if ( !tooltipElement ) return 0
+    return tooltipElement.clientHeight
+})
+
+const tooltipWidth = computed(() => {
+    const tooltipElement = document.getElementById(tooltipId.value)
+    if ( !tooltipElement ) return 0
+    return tooltipElement.clientWidth
+})
+
+const positionLeft = () => {
+    const targetElement = document.getElementById(id.value)
+    if ( !targetElement ) return
+    const clientRect = targetElement.getBoundingClientRect()
+    const x = clientRect.left
+    const elementWidth = targetElement.clientWidth
+    if ( props.location === 'start' ) return x - tooltipWidth.value - 8 + 'px'
+    if ( props.location === 'end' ) return x + elementWidth + 10 + 'px'
+    return x + (elementWidth/2 - tooltipWidth.value/2) + 'px'
+}
+
+const positionTop = () => {
+    const targetElement = document.getElementById(id.value)
+    if ( !targetElement ) return
+    const clientRect = targetElement.getBoundingClientRect()
+    const y = clientRect.top
+    const elementHeight = targetElement.clientHeight
+    if ( props.location === 'top' ) return y - tooltipHeight.value - 8 + 'px'
+    if ( props.location === 'bottom' ) return y + elementHeight + 8 + 'px'
+    return y + (elementHeight/2 - tooltipHeight.value/2) + 'px'
 }
 </script>
 <template>
-<div :class="$style.tooltip">
-    <span v-show="data.isActive" :class="$style.message" :style="styles()">
-        {{msg}}
-    </span>
+<div class="relative inline-flex">
     <div 
+    :id="id"
     @mouseenter="open"
     @mouseleave="close"
     @focus.capture="open"
     @blur.capture="close"
-    :class="$style.wrapperContent"
     >
-        <slot/>
+        <slot name="activator"/>
     </div>
+    <Teleport :to="teleportTarget">
+        <span :id="tooltipId" :class="computedClass" :style="{left: positionLeft(), top: positionTop()}">
+            <slot>
+                {{text}}
+            </slot>
+        </span>
+    </Teleport>
 </div>
 </template>
-<style module>
-.tooltip {
-    position: relative;
-}
-.message {
-    position: absolute;
-    width: auto;
-    border-radius: 10px;
-    background-color: rgb(65, 65, 65);
-    padding: 0.25rem 0.5rem;
-    color: white;
-    font-size: 0.5rem;
-    white-space: nowrap;
-    z-index: 9999;
-}
-.wrapperContent {
-    cursor: pointer;
-}
-
-@media screen and (max-width: 600px) {
-    .message {
-        display: none;
-    }
-}
-</style>
