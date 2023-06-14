dcff5e9d05638d134b4c4462c26ad0c6c25b34a9
riyo kondo <r_kondo@actier.co.jp>
2023-06-14 14:37:01 +0900

    不要なコード(teleport)を整理

--- src/composables/teleport.ts
+++ src/composables/teleport.ts
@@ -3,8 +3,5 @@ import { computed } from "vue"
 export const teleportTarget = computed(() => {
     if (typeof window === 'undefined') return undefined
     const targetElement = document.body
-    const container = document.createElement('div')
-    container.className = 'c-overlay-container'
-    targetElement.appendChild(container)
-    return container
+    return targetElement
 })
