6b3298b96aa57430641eca0a8894b7df82d5c391
Kiminobu Taniguchi <k_taniguchi@aciter.co.jp>
2023-06-16 13:09:00 +0900

    新しく作成したコンポーネントを公開

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import './tailwind.css'
 import './variables.css'
+import './axioms.css'
 
 // containment
 export {default as CButton} from "./components/containment/CButton.vue"
+export {default as CChip} from "./components/containment/CChip.vue"
 export {default as CDialog} from "./components/containment/CDialog.vue"
 export {default as COverlay} from "./components/containment/COverlay.vue"
 export {default as CSheet} from "./components/containment/CSheet.vue"
+export {default as CTooltip} from "./components/containment/CTooltip.vue"
 //data display
+export {default as CDataTable} from "./components/dataDisplay/CDataTable.vue"
 export {default as CTable} from "./components/dataDisplay/CTable.vue"
 // feedback
+export {default as CAlert} from "./components/feedback/CAlert.vue"
 export {default as CProgress} from "./components/feedback/CProgress.vue"
 // form
 export {default as CAutocomplete} from "./components/form/CAutocomplete.vue"
@@ -17,6 +22,7 @@ export {default as CSelect} from "./components/form/CSelect.vue"
 export {default as CTextarea} from "./components/form/CTextarea.vue"
 export {default as CTextField} from "./components/form/CTextField.vue"
 //images
+export {default as CAvatar} from "./components/images/CAvatar.vue"
 export {default as CSvgIcon} from "./components/images/CSvgIcon.vue"
 // layout
 export {default as CBox} from "./components/layout/CBox.vue"
