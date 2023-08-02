a78b29291a114b403cd3600235f05c50e6706a51
riyo kondo <r_kondo@actier.co.jp>
2023-08-02 17:07:49 +0900

    disabled対応の修正

--- src/components/navigation/CTab.vue
+++ src/components/navigation/CTab.vue
@@ -25,22 +25,24 @@ const props = withDefaults(defineProps<{
 
 const tabClass = computed(() => {
     return [
-        'border-b border-gray-300 aria-[selected=false]:!text-current -m-[1px] hover:bg-black/20 focus:bg-black/20',
+        'border-b border-gray-300 -m-[1px] hover:bg-black/20 focus:bg-black/20 aria-selected:border-b-2',
+        props.disabled ? '' : 'aria-[selected=false]:!text-current',
         colorClass.value,
     ]
 })
 
 const colorClass = computed(() => {
-    if ( props.color === 'white' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-white)] aria-selected:text-[var(--cuv-white)]'
-    if ( props.color === 'black' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-black)] aria-selected:text-[var(--cuv-black)]'
-    if ( props.color === 'light' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-light-border)] aria-selected:text-[var(--cuv-light-text)]'
-    if ( props.color === 'dark' )  return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-dark-border)] aria-selected:text-[var(--cuv-dark-text)]'
-    if ( props.color === 'primary' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-primary-border)] aria-selected:text-[var(--cuv-primary-text)]'
-    if ( props.color === 'link' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-link-border)] aria-selected:text-[var(--cuv-link-text)]'
-    if ( props.color === 'success' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-success-border)] aria-selected:text-[var(--cuv-success-text)]'
-    if ( props.color === 'danger' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-danger-border)] aria-selected:text-[var(--cuv-danger-text)]'
-    if ( props.color === 'warning' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-warning-border)] aria-selected:text-[var(--cuv-warning-text)]'
-    if ( props.color === 'info' ) return 'aria-selected:border-b-2 aria-selected:border-[var(--cuv-info-border)] aria-selected:text-[var(--cuv-info-text)]'
+    if ( props.disabled ) return ''
+    if ( props.color === 'white' ) return 'aria-selected:border-[var(--cuv-white)] aria-selected:text-[var(--cuv-white)]'
+    if ( props.color === 'black' ) return 'aria-selected:border-[var(--cuv-black)] aria-selected:text-[var(--cuv-black)]'
+    if ( props.color === 'light' ) return 'aria-selected:border-[var(--cuv-light-border)] aria-selected:text-[var(--cuv-light-text)]'
+    if ( props.color === 'dark' )  return 'aria-selected:border-[var(--cuv-dark-border)] aria-selected:text-[var(--cuv-dark-text)]'
+    if ( props.color === 'primary' ) return 'aria-selected:border-[var(--cuv-primary-border)] aria-selected:text-[var(--cuv-primary-text)]'
+    if ( props.color === 'link' ) return 'aria-selected:border-[var(--cuv-link-border)] aria-selected:text-[var(--cuv-link-text)]'
+    if ( props.color === 'success' ) return 'aria-selected:border-[var(--cuv-success-border)] aria-selected:text-[var(--cuv-success-text)]'
+    if ( props.color === 'danger' ) return 'aria-selected:border-[var(--cuv-danger-border)] aria-selected:text-[var(--cuv-danger-text)]'
+    if ( props.color === 'warning' ) return 'aria-selected:border-[var(--cuv-warning-border)] aria-selected:text-[var(--cuv-warning-text)]'
+    if ( props.color === 'info' ) return 'aria-selected:border-[var(--cuv-info-border)] aria-selected:text-[var(--cuv-info-text)]'
     return 'border-inherit'
 })
 
