aeeb52ab5e4962e7868da1369b857a4f62716134
riyo kondo <r_kondo@actier.co.jp>
2023-08-04 14:58:49 +0900

    CBreadcrumbsコンポーネントの作成

--- src/components/navigation/CBreadcrumbs.story.vue
+++ src/components/navigation/CBreadcrumbs.story.vue
@@ -1,26 +1,77 @@
 <script setup lang="ts">
 import { reactive } from "vue";
+import { mdiArrowRight, mdiChevronRight } from '@mdi/js';
 import CBreadcrumbs from "@/components/navigation/CBreadcrumbs.vue";
+import CSvgIcon from "@/components/images/CSvgIcon.vue";
 
 type ColorType =
     'white' | 'black' | 'light' | 'dark' | 
     'primary' | 'link' |
     'success' | 'danger' | 'warning' | 'info'
 
-const page = [
+  interface itemsType {
+    title: string,
+    disabled?: boolean,
+    href?: string
+}
+
+
+const pageStringList = [
   'Home',
   'Doc',
   'Breadcrumb'
 ]
 
+const pageObjectList = [
+{
+    title: 'Home',
+    disabled: false,
+    href: 'link_home'
+  },
+  {
+    title: 'Doc',
+    disabled: false,
+    href: 'link_doc'
+  },
+  {
+    title: 'Breadcrumb',
+    disabled: true,
+    href: 'link_breadcrumbs'
+  },
+]
+
+const pageObjectRouterLinkList = [
+{
+    title: 'Home',
+    disabled: false,
+    to: 'link_home'
+  },
+  {
+    title: 'Doc',
+    disabled: false,
+    to: 'link_doc'
+  },
+  {
+    title: 'Breadcrumb',
+    disabled: true,
+    to: 'link_breadcrumbs'
+  },
+]
+
 const data: {
   bgColor: ColorType | undefined
-  items: string[],
+  color: ColorType | undefined
+  items: (string | itemsType)[],
+  density: 'default' | 'comfortable' | 'compact'
   divider: string
+  rounded: 'none' | 'small' | 'medium' | 'large' | 'x-large' | 'circle'
 } = reactive({
   bgColor: undefined,
-  items: page,
+  color: undefined,
+  items: pageObjectList,
+  density: 'default',
   divider: '/',
+  rounded: 'none',
 })
 
 </script>
@@ -30,11 +81,14 @@ const data: {
       title="Navigation / CBreadcrumbs"
       :layout="{ type: 'grid', width: '100%' }"
   >
-    <Variant title="基本" auto-props-disabled>
+  <Variant title="基本" auto-props-disabled>
       <CBreadcrumbs
-        :items="data.items"
-        :divider="data.divider"
         :bg-color="data.bgColor"
+        :color="data.color"
+        :density="data.density"
+        :divider="data.divider"
+        :items="data.items"
+        :rounded="data.rounded"
       />
       <template #controls>
         <HstSelect
@@ -54,68 +108,126 @@ const data: {
                         {value: 'info', label: 'info'},
                     ]"
         />
+        <HstSelect
+            v-model="data.color"
+            title="color"
+            :options="[
+                        {value: undefined, label: 'undefined'},
+                        {value: 'white', label: 'white'},
+                        {value: 'black', label: 'black'},
+                        {value: 'light', label: 'light'},
+                        {value: 'dark', label: 'dark'},
+                        {value: 'primary', label: 'primary'},
+                        {value: 'link', label: 'link'},
+                        {value: 'success', label: 'success'},
+                        {value: 'danger', label: 'danger'},
+                        {value: 'warning', label: 'warning'},
+                        {value: 'info', label: 'info'},
+                    ]"
+        />
+        <HstSelect
+          v-model="data.density"
+          title="density"
+          :options="[
+                      {value: 'default', label: 'default'},
+                      {value: 'comfortable', label: 'comfortable'},
+                      {value: 'compact', label: 'compact'},
+                  ]"
+          />
+        <HstText v-model="data.divider" title="divider"/>
+        <HstSelect
+            v-model="data.rounded"
+            title="rounded"
+            :options="[
+                        {value: 'none', label: 'none'},
+                        {value: 'small', label: 'small'},
+                        {value: 'medium', label: 'medium'},
+                        {value: 'large', label: 'large'},
+                        {value: 'x-large', label: 'x-large'},
+                        {value: 'circle', label: 'circle'},
+                    ]"
+        />
+        <HstJson v-model="data.items" title="items"/>
       </template>
+    </Variant>
+    <Variant title="router-link" auto-props-disabled>
+      <CBreadcrumbs
+        :items="pageObjectRouterLinkList"
+      />
+    </Variant>
+    <Variant title="Props Divider" auto-props-disabled>
+      <CBreadcrumbs
+        :items="pageStringList"
+        divider="-"
+      />
+      <CBreadcrumbs
+        :items="pageStringList"
+        divider="."
+      />
+    </Variant>
+    <Variant title="Slots Divider" auto-props-disabled>
+      <CBreadcrumbs
+        :items="pageStringList"
+      >
+        <template #divider>
+          <CSvgIcon :icon="mdiArrowRight"/>
+        </template>
+      </CBreadcrumbs>
+      <CBreadcrumbs
+        :items="pageStringList"
+      >
+        <template #divider>
+          <CSvgIcon :icon="mdiChevronRight"/>
+        </template>
+      </CBreadcrumbs>
+    </Variant>
+    <Variant title="Slots Title" auto-props-disabled>
+      <CBreadcrumbs
+        :items="pageStringList"
+      >
+        <template v-slot:title="{ item }">
+          {{ item.toUpperCase() }}
+        </template>
+      </CBreadcrumbs>
     </Variant>
   </Story>
 </template>
 
 <docs lang="md">
-# CTabs / CTab
+# CBreadcrumbs
 
-複数のタブを作成することで、ページ上のコンテンツを整理することが可能なコンポーネントです。
+ユーザーが今WEBサイト上のどこにいるのかを伝えることができる、階層順のリストコンポーネントです。またはパンくずリストとも呼ばれます。
 
 ## Props
-* colorType = 'white' \| 'black' \| 'light' \| 'dark' \| 'primary' \| 'link' \| 'success' \| 'danger' \| 'warning' \| 'info'
+*1 colorType = 'white' \| 'black' \| 'light' \| 'dark' \| 'primary' \| 'link' \| 'success' \| 'danger' \| 'warning' \| 'info'
+*2 itemType = {
+  title: string
+  disabled?: boolean
+  href?: string
+  to?: string
+}
 
-### CTabs
 | Name | Type | Default | Description |
 | --- | --- | --- | --- |
-| alignTabs | 'end' \| 'start' \| 'center' | 'start' | コンポーネントの位置を指定します |
-| bgColor | colorType* | 'white' | `background-color`を指定します |
-| color | colorType | 'black' | 選択されているタブの`color`を指定します |
+| bgColor | colorType* | undefined | `background-color`を指定します |
+| color | colorType* | undefined | titleの`color`を指定します |
 | density | 'default' \| 'comfortable' \| 'compact' | 'default' | コンポーネントが使用する垂直方向の高さを調整します |
-| disabled | boolean | false | 非活性にする場合は指定します |
-| grow | boolean | false | 可能な範囲で全てのスペースに広がるかどうかを指定します |
-| items | string[] | undefined | タブに表示するテキストを配列で渡します。この値は`value`の値としても使用されます。 |
-| modelValue | string | '' | v-model値を指定します |
-| nextIcon | string | undefined | `show-arrows`がtrueの場合、右に表示されるアイコンをカスタムすることができます |
-| prevIcon | string | undefined | `show-arrows`がtrueの場合、左に表示されるアイコンをカスタムすることができます |
-| showArrows | boolean | false | タブの左右に矢印を表示させる場合は指定します |
-
-### CTab
-| Name | Type | Default | Description |
-| --- | --- | --- | --- |
-| appendIcon | string | undefined | コンポーネントの右側に表示させるiconを指定します |
-| color | colorType | 'black' | 選択されているタブの`color`を指定します |
-| density | 'default' \| 'comfortable' \| 'compact' | 'default' | コンポーネントが使用する垂直方向の高さを調整します |
-| disabled | boolean | false | 非活性にする場合は指定します |
-| elevation | 'small' \| 'medium' \| 'large' | undefined | 影をつける場合に指定します |
-| icon | string | undefined | iconを指定します |
-| prependIcon | string | undefined | コンポーネントの左側に表示させるiconを指定します |
-| value | string \| number | undefined | valueの値を指定します |
-
+| divider | string | '/' | 階層を区切る文字列を指定します |
+| items | (string \| itemsType)[] | undefined | Breadcrumbsを自動生成するために使用される文字列またはオブジェクトの配列を渡します |
+| rounded | 'none' \| 'small' \| 'medium' \| 'large' \| 'x-large' \| 'circle' | 'none' | コンポーネントの`border-radius`を指定します |
 
 ## Slots
 
-### CTabs
 | Name | Props (if scoped) | Description |
 | --- | --- | --- |
-| default | - | CTabsに表示するコンテンツを指定します |
-
-### CTab
-| Name | Props (if scoped) | Description |
-| --- | --- | --- |
-| default | - | CTabに表示するコンテンツを指定します |
+| title | `{index: number, item: string\|itemType}` | title部分に表示するコンテンツを表示します |
+| divider | `{index: number, item: string\|itemType}` | パンくずの区切り部分に表示するコンテンツを表示します |
 
 ## Events
 
 ### CTabs
 | Name | Parameters | Description |
 | --- | --- | --- |
-| update:modelValue | - | コンポーネントのv-modelが変更されたときに発行されるイベントです |
-
-### CTab
-| Name | Parameters | Description |
-| --- | --- | --- |
 | - | - | このコンポーネント独自のイベントはありません |
+
 </docs>
--- src/components/navigation/CBreadcrumbs.vue
+++ src/components/navigation/CBreadcrumbs.vue
@@ -7,22 +7,48 @@ type ColorType =
     'primary' | 'link' |
     'success' | 'danger' | 'warning' | 'info'
 
+interface itemsType {
+    title: string
+    disabled?: boolean
+    href?: string
+    to?: string
+}
+
 const props = withDefaults(defineProps<{
     bgColor?: ColorType
-    items: string[]
+    color?: ColorType
     density?:'default' | 'comfortable' | 'compact'
     divider?: string
+    items: (string | itemsType)[]
+    rounded?: 'none' | 'small' | 'medium' | 'large' | 'x-large' | 'circle'
 }>(), {
     density: 'default',
     divider: '/',
+    rounded: 'none',
 })
 
 const ulClass = computed(() => {
-    return [
+    const base = [
         'flex items-center leading-[1.375rem] px-3',
         fixedPadding.value,
+        fixedRounded.value,
         props.bgColor ? useVariant({variant: 'flat', color: props.bgColor}) : '',
     ]
+    return base
+})
+
+const colorClass = computed(() => {
+    if ( props.color === 'white' ) return '!text-[var(--cuv-white)]'
+    if ( props.color === 'black' ) return '!text-[var(--cuv-black)]'
+    if ( props.color === 'light' ) return '!text-[var(--cuv-light-text)]'
+    if ( props.color === 'dark' )  return '!text-[var(--cuv-dark-text)]'
+    if ( props.color === 'primary' ) return '!text-[var(--cuv-primary-text)]'
+    if ( props.color === 'link' ) return '!text-[var(--cuv-link-text)]'
+    if ( props.color === 'success' ) return '!text-[var(--cuv-success-text)]'
+    if ( props.color === 'danger' ) return '!text-[var(--cuv-danger-text)]'
+    if ( props.color === 'warning' ) return '!text-[var(--cuv-warning-text)]'
+    if ( props.color === 'info' ) return '!text-[var(--cuv-info-text)]'
+    return 'text-inherit'
 })
 
 const fixedPadding = computed(() => {
@@ -30,6 +56,43 @@ const fixedPadding = computed(() => {
     if ( props.density === 'compact' ) return 'py-2'
     return 'py-4'
 })
+
+const fixedRounded = computed(() => {
+    if ( props.rounded === 'small' ) return 'rounded-sm'
+    if ( props.rounded === 'medium' ) return 'rounded'
+    if ( props.rounded === 'large' ) return 'rounded-lg'
+    if ( props.rounded === 'x-large' ) return 'rounded-3xl'
+    if ( props.rounded === 'circle' ) return 'rounded-full'
+    if ( props.rounded === 'none' ) return 'rounded-none'
+    return 'rounded-none'
+})
+
+const isTag = (item: itemsType) => {
+    if ( 'href' in item ) return 'a'
+    if ( 'to' in item ) return 'router-link'
+    return ''
+}
+
+const formatTitle = (item: string|itemsType) => {
+    if ( typeof item === 'string' ) return item
+    return item.title
+}
+
+const disabledClass = (item: string|itemsType, index: number) => {
+    if ( typeof item === 'string' && index === props.items.length-1 ) return 'pointer-events-none opacity-40'
+    if ( typeof item === 'string' ) return ''
+    if ( item.disabled ) return 'pointer-events-none opacity-40'
+    return ''
+}
+
+const isDefaultItem = (item: string|itemsType) => {
+    if ( typeof item === 'string' ) return true
+    if ( 'href' in item ) return false
+    if ( 'to' in item ) return false
+    return true
+}
+
+
 </script>
 
 <template>
@@ -40,15 +103,26 @@ const fixedPadding = computed(() => {
                 {{ divider }}
             </slot>
         </li>
-        <li v-if="index !== items.length-1" class="items-center inline-flex px-1 text-inherit">
-            <slot name="title" :item="item" :index="index">
-                {{ item }}
-            </slot>
-        </li>
-        <li v-if="index === items.length-1" class="items-center inline-flex px-1 text-inherit pointer-events-none opacity-40">
-            <slot name="title" :item="item" :index="index">
-                {{ item }}
-            </slot>
+        <li :class="[colorClass, disabledClass(item, index)]" class="items-center inline-flex px-1 text-inherit">
+            <template v-if="isDefaultItem(item)" >
+                <slot name="title" :item="item" :index="index">
+                    {{ formatTitle(item) }}
+                </slot>
+            </template>
+            <template v-if="typeof item !== 'string' && ('href' in item)">
+                <component :is="isTag(item)" :href="item.href ? item.href : undefined" class="hover:underline">
+                    <slot name="title" :item="item" :index="index">
+                        {{ formatTitle(item) }}
+                    </slot>
+                </component>
+            </template>
+            <template v-if="typeof item !== 'string' && ('to' in item)">
+                <component :is="isTag(item)" :to="item.to ? item.to : undefined" class="hover:underline cursor-pointer">
+                    <slot name="title" :item="item" :index="index">
+                        {{ formatTitle(item) }}
+                    </slot>
+                </component>
+            </template>
         </li>
     </template>
 </ul>
