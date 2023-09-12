3605caa919095eea64c9a210822b519d76c20040
riyo kondo <r_kondo@actier.co.jp>
2023-09-12 14:23:51 +0900

    imagesの修正

--- src/components/images/CAvatar.vue
+++ src/components/images/CAvatar.vue
@@ -1,3 +1,9 @@
+<script lang="ts">
+export default {
+    name: 'CAvatar',
+}
+</script>
+
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useVariant } from '../../composables/variant'
